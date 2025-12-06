121859858569a77781ff09c68ec123cc02099d09
gusgh075 <gusgh075@gmail.com>
2025-12-06 17:26:32 +0900

    feat : itemAddServiceimple.java에 itemAdd기능 구현.

--- src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
+++ src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
@@ -2,18 +2,21 @@ package com.minijava.service.impl;
 
 import com.minijava.dto.ItemDTO;
 import com.minijava.repository.ItemRepository;
+import com.minijava.repository.ItemRepositoryInterface;
 import com.minijava.service.ItemAddService;
 
 public class ItemAddServiceImpl implements ItemAddService {
-    final ItemRepository itemRepository;
-    ItemAddServiceImpl(ItemRepository itemRepository){
-        this.itemRepository=itemRepository;
+    final ItemRepositoryInterface itemRepository;
+
+    ItemAddServiceImpl(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
 
     @Override
     public int itemAdd(String name, String rank, String quality, int power) {
-
-        return 0;
+        int newItemNumber = itemRepository.getLastItemNumber() + 1;
+        itemRepository.save(new ItemDTO(newItemNumber, name, rank, quality, power));
+        return newItemNumber;
     }
 }
